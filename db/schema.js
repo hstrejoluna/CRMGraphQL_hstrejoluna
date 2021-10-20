29431a9e110ef99b33dcfa32b2471b83e6775818
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-20 13:58:00 -0500

    remove token from schema of query get user

--- db/schema.js
+++ db/schema.js
@@ -109,7 +109,7 @@ const typeDefs = gql`
 
   type Query {
     #Usuarios
-    obtenerUsuario(token: String!): Usuario
+    obtenerUsuario: Usuario
 
     #Productos
     obtenerProductos: [Producto]
